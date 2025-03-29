7efd23872b133a2c942eaefcbecd19ff5a7670ed
niri81 <38499069+niri81@users.noreply.github.com>
2025-03-29 15:47:46 +0100

    refactor: change shorthand flag for config file

--- cmd/root.go
+++ cmd/root.go
@@ -26,5 +26,5 @@ func Execute() {
 }
 
 func init() {
-	rootCmd.PersistentFlags().StringVarP(&cfgFile, "config", "p", "./config.json", "config file, either JSON or YAML")
+	rootCmd.PersistentFlags().StringVarP(&cfgFile, "config", "c", "./config.json", "config file, either JSON or YAML")
 }
